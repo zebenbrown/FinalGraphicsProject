b17b75e7065e5cc49e03060e730b7131f0288be4
zebenbrown <zeben.brown@mymail.champlain.edu>
2024-11-22 22:19:02 -0500

    made progress on adding text rendering

--- core/cobb/textRendering.cpp
+++ core/cobb/textRendering.cpp
@@ -3,3 +3,62 @@
 //
 
 #include "textRendering.h"
+
+void textRendering::loadText(std::string fontPath)
+{
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Failed tp initialize freetype" << std::endl;
+    }
+
+    //find path to font
+    if (fontPath.empty())
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font name" << std::endl;
+    }
+
+    //load font as face
+    if (FT_New_Face(ft, fontPath.c_str(), 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+    }
+    else
+    {
+        //set size to load glyphs as
+        FT_Set_Pixel_Sizes(face, 0, 48);
+
+        //disable byte-alignment restriction
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+        //load first 128 characters of ASCII set
+        for (unsigned char c = 0; c < 128; c++)
+        {
+            //load character glyph
+            if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+            {
+                std::cout << "ERROR::FREETYPE: Failed to load glyph" << std::endl;
+                continue;
+            }
+
+            //generate texture
+            unsigned int glyphTexture;
+            glGenTextures(1, &glyphTexture);
+            glBindTexture(GL_TEXTURE_2D, glyphTexture);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, face->glyph->bitmap.width, face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE, face->glyph->bitmap.buffer);
+
+            //set texture options
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+            //store character for later use
+            Character character =
+                    {
+                    glyphTexture, glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows), glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top), static_cast<unsigned int>(face->glyph->advance.x)
+                    };
+            characterMap.insert(std::pair<char, Character>(c, character));
+        }
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+}
--- core/cobb/textRendering.h
+++ core/cobb/textRendering.h
@@ -9,10 +9,33 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
+#include "../ew/external/glad.h"
+#include <glm/glm.hpp>
+#include <iostream>
+#include <map>
+
 
 class textRendering {
+public:
 
-};
+    struct Character {
+        unsigned int textureID; //ID handle of glyph texture
+        glm::ivec2 Size; //Size of glyph
+        glm::ivec2 Bearing; //Offset from baseline to top/left of glyph
+        unsigned int Advance; //Horizontal offset to advance to next glyph
+    };
 
+    FT_Library ft;
+    FT_Face face;
+    std::map<GLchar, Character> characterMap;
+
+    void loadText(std::string fontPath);
+
+    ~textRendering()
+    {
+        FT_Done_Face(face);
+        FT_Done_FreeType(ft);
+    }
 
 #endif //ITMG_FINAL_TEXTRENDERING_H
+};
